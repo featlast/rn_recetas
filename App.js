5975f4736d2edd5b43d167e300371b8a19794691
Jose Herrera <jherrera892016@gmail.com>
2022-08-20 15:21:48 -0500

    refactor(core): Add refactor Components
    - Add Stack Navigation.
    - Add Home Component.
    - Refactor Code Components.
    
    co-authored-by: featlast <jherrera892016@gmail.com>

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import MyStack from './src/Navigations/MyStack';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor={'red'} />
-      <Text style={styles.sectionTitle}>Welcome REACT NATIVE CLI</Text>
-    </SafeAreaView>
+    <MyStack/>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
 
 export default App;
--- src/Navigations/MyStack.jsx
+++ src/Navigations/MyStack.jsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import  { Home } from '../Components/Home'
+
+const Stack = createNativeStackNavigator()
 
 const MyStack = () => {
-  return (
-    <View>
-      <Text>MyStack</Text>
-    </View>
-  )
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+    )
 }
 
 export default MyStack
